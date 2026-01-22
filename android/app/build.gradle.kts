d262bffd613b040ed799e1f0b3d395f55cde45dc
Juan P. Torres <est.juanpablotorres@gmail.com>
2026-01-22 15:34:14 -0400

    Bump version to 1.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.ridepulse.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.1.1"
     }
 
     signingConfigs {
